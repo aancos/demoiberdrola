42e9438c9c2a1d9d5fae2650227d91344edc8449
s4dev-a00@education.cloud.sap <s4dev-a00@education.cloud.sap>
2024-11-26 12:43:37 +0000

    Subida inicial

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("sap.training.bookings00.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
